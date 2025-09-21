e56b3a56e3ec82e64e6fa8497fc19439cd844b31
Luan Bruno Nalati <nallu.bruno@gmail.com>
2025-09-21 12:02:41 -0300

    botão salvar cadastrando produto

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -18,12 +18,32 @@ import java.util.ArrayList;
 public class ProdutosDAO {
     
     Connection conn;
+    conectaDAO conexao;
     PreparedStatement prep;
     ResultSet resultset;
     ArrayList<ProdutosDTO> listagem = new ArrayList<>();
     
+    public ProdutosDAO(){
+        this.conexao = new conectaDAO();
+        this.conn = this.conexao.getConexao();
+    }
+    
+    
     public void cadastrarProduto (ProdutosDTO produto){
         
+         String sql = "INSERT INTO produtos (nome,valor, status) VALUES" + "(?, ?, ?)";
+         
+        try {
+        PreparedStatement stmt = this.conn.prepareStatement(sql);
+            stmt.setString(1, produto.getNome());
+            stmt.setInt(2,produto.getValor());
+            stmt.setString(3, produto.getStatus());
+            stmt.execute();
+        } catch (Exception e){
+         System.out.println("Erro ao inserir empresa: " + e.getMessage());
+        
+        
+        }
         
         //conn = new conectaDAO().connectDB();
         
